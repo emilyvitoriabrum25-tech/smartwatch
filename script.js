7bfa3087dead1e4a1cb488ddfdc8de857896aefa
emyvic <238736561+emilyvitoriabrum25-tech@users.noreply.github.com>
2026-06-18 16:07:43 -0300

    Atualizando o JavaScript, coloquei 'PM' pra horas e adicionei mes e dias da semana pra mudar automatixamente

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-const visor = document.querySelector('.bloco-horario h2');
 function atualizarRelogio() {
-    let agora = new Date();
+    const agora = new Date();
     let horas = agora.getHours();
-    let minutos = agora.getMinutes();
-    visor.textContent = horas + ":" + minutos;
+    const minutos = String(agora.getMinutes()).padStart(2, '0');
+    const amPm = horas >= 12 ? 'PM' : 'AM';
+    horas = horas % 12;
+    horas = horas === 0 ? 12 : horas;
+    horas = String(horas).padStart(2, '0');
+    visor.childNodes[0].textContent = `${horas}:${minutos} `;
+    periodo.textContent = amPm;
+
+    const diasSemana = [
+        'SUNDAY',
+        'MONDAY',
+        'TUESDAY',
+        'WEDNESDAY',
+        'THURSDAY',
+        'FRIDAY',
+        'SATURDAY'
+    ];
+
+    const meses = [
+        'JAN',
+        'FEB',
+        'MAR',
+        'APR',
+        'MAY',
+        'JUN',
+        'JUL',
+        'AUG',
+        'SEP',
+        'OCT',
+        'NOV',
+        'DEC'
+    ];
+
+    const diaSemana = diasSemana[agora.getDay()];
+    const mes = meses[agora.getMonth()];
+    const dia = agora.getDate();
+
+    dataAtual.textContent = `${diaSemana} ${mes} ${dia}`;
 }
-
-atualizarRelogio();
-
-setInterval(atualizarRelogio, 1000);
